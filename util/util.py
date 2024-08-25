f430e400a2d7524012b1d89be6dca56d54b80337
albert <you@example.com>
2024-08-25 17:17:46 -0500

    add scraping

--- util/util.py
+++ util/util.py
@@ -1,2 +1,10 @@
-def log():
-    print('hello')
+import os
+import pickle
+
+
+def write_data(data, filename):
+    full_path = os.path.join("data", filename)
+    if not os.path.exists("data"):
+        os.makedirs("data")
+
+    pickle.dump(data, open(full_path, "wb"))
